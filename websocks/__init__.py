4c2b4a9e1fe7f46d3e0ba67fb15f4db284dbd2e1
abersheeran <me@abersheeran.com>
2020-02-02 14:32:15 +0800

    delete IOCP

--- websocks/__init__.py
+++ websocks/__init__.py
@@ -1,12 +1,6 @@
 import sys
 import asyncio
 
-if sys.platform == "win32":  # use IOCP in windows
-    if sys.version_info.major >= 3 and sys.version_info.minor >= 7:
-        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-    else:
-        asyncio.set_event_loop(asyncio.ProactorEventLoop())
-
 try:
     import uvloop
 
