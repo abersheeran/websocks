dd246882523a955ac960d748906cea02d4774b9e
Aber <me@abersheeran.com>
2021-01-02 04:07:49 +0800

    Fix aiodns error with ProactorEventLoop

--- websocks/__init__.py
+++ websocks/__init__.py
@@ -4,3 +4,15 @@ try:
     uvloop.install()
 except ImportError:
     pass
+
+if (
+    sys.version_info.major >= 3
+    and sys.version_info.minor >= 8
+    and platform.system() == "Windows"
+):
+    selector = selectors.SelectSelector()
+    loop = asyncio.SelectorEventLoop(selector)
+    asyncio.set_event_loop(loop)
+else:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
